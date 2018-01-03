f065f57b12404585552f400d7a87dfd31420758c
Neil Marklund <nmarklund10@hotmail.com>
2018-01-03 21:24:46 +0800

    Almost working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,14 +12,32 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    if (!params[:ratings].nil?)
-      Movie.checked_ratings = params[:ratings].keys
+    
+    if (params[:ratings].nil? || params[:str].nil?)
+      if (params[:ratings].nil?)
+        @ratings_param = session[:ratings]
+      else
+        @ratings_param = params[:ratings]
+      end
+      if (params[:str].nil?)
+        @str_param = session[:str]
+      else
+        @str_param = params[:str]
+      end
+      redirect_to movies_path, :str => str_param, :ratings =>ratings_param
     end
-    @ratings_checked = Movie.checked_ratings
+    if (!params[:ratings].nil?)
+      session[:checked_ratings] = params[:ratings].keys
+    end
+    @ratings_checked = session[:checked_ratings]
     @displayed_movies = Movie.where({ rating: @ratings_checked})
-    if (params[:str] == "movie")
+    if (!params[:str].nil?)
+      session[:str] = params[:str]
+    end
+    @ordering = session[:str]
+    if (@ordering == "movie")
       @movies = @displayed_movies.order(:title)
-    elsif (params[:str] == "release")
+    elsif (@ordering == "release")
       @movies = @displayed_movies.order(:release_date)
     else
       @movies = @displayed_movies
